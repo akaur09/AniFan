fc6dd7599c978e5eaa84993a5bf945b084172590
akaur091 <amniderkaur.09@gmail.com>
2022-05-10 18:05:24 -0700

    add model code for anime.js

--- models/anime.js
+++ models/anime.js
@@ -7,7 +7,25 @@ Anime.init(
     {
         id:{
             type: DataTypes.INTEGER,
-            
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title:{
+            type: DataTypes.String,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
         }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'anime'
     }
 )
