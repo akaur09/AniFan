120a3305b6f3b6fe8c08a9402bbb24f17f23ebc0
alex hernandez <alexanderohernandez21@gmail.com>
2022-05-12 11:03:27 -0700

    added comment, review, and user routes

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -3,6 +3,93 @@ const { User, Review, Comment, Anime } = require('../../models');
 const withAuth = require('../../utils/auth');
 const sequelize = require('../../config/connection');
 
+router.get('/', async(req, res)=>{
+    try{
+        const reviewData = await Review.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    include: {
+                        model: User
+                    }
+                },
+            ]
+        });
+        res.status(200).json(reviewData);
+    }catch(err){
+        res.status(500).json(err);
+    };
+});
 
+router.get('/:id', async (req, res)=>{
+    try{
+        const reviewData = await Review.findByPk(req.params.id, {
+            include:[
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    include:{
+                        model: User
+                    }
+                },
+            ]
+        });
+        if(!reviewData){
+            res.status(404).json({message: 'No review found with this id!'});
+            return;
+        }else{
+            res.status(200).json(reviewData)
+        }
+    }catch(err){
+        res.status(500).json(err);
+    };
+});
+
+router.post('/', withAuth, async (req, res)=>{
+    try{
+        const reviewData = await Review.update({
+            title: req.body.title,
+            review_text: req.body.review_text
+        },
+        {
+            where:{
+                id: req.params.id
+            }
+        });
+        if(!reviewData){
+            res.status(404).json({message: 'No review found with this id!'});
+            return;
+        }else{
+            res.status(200).json(reviewData);
+        };
+    }catch(err){
+        res.status(500).json(err);
+    };
+});
+
+router.delete('/:id',withAuth, async(req,res)=>{
+    try{
+        const reviewData = await Review.destroy({
+            where:{
+                id: req.params.id
+            }
+        });
+        if(!reviewData){
+            res.status(404).json({message: 'No review found with this id!'});
+            return;
+        }else{
+            res.status(200).json(reviewData);
+        }
+    }catch(err){
+        res.status(500).json(err);
+    };
+});
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,6 +3,49 @@ const { User, Review, Comment, Anime } = require('../../models');
 const withAuth = require('../../utils/auth');
 const sequelize = require('../../config/connection');
 
+router.post('/', async (req, res)=>{
+    try{
+        const userData = await User.create(req.body);
 
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+
+        res.status(200).json(userData);
+    }catch(err){
+        res.status(400).json(err);
+    };
+});
+
+router.post('/login',async (req,res)=>{
+    try{
+        const userData = await User.findOne({where: {username: req.body.username}});
+        if(!userData){
+            res.status(400).json({message: 'Incorrect Username'});
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json({message: 'Incorrect Password'});
+            return;
+        }
+        req.session.save(()=>{
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({user: userData, message: 'you are now logged in!'})
+        })
+    }catch(err){
+        res.status(400).json(err);
+    };
+});
+
+router.post('/logout',(req, res)=>{
+    if(req.session.logged_in){
+        req.session.destroy(()=>{
+            res.status(204).end();
+        });
+    }else{
+        res.status(404).end
+    }
+})
 
 module.exports = router;
