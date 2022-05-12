94f4050d00df639c164cb349813ac1d0e0d1b99d
luccaloopz <luccamartins2012@gmail.com>
2022-05-12 10:34:48 -0700

    homepage now displays trending anime card poster images and titles.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,45 @@
 const router = require('express').Router();
+const axios = require('axios').default;
 const { User, Review, Comment, Anime } = require('../models');
 const withAuth = require('../utils/auth');
 const sequelize = require('../config/connection');
 
 router.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://kitsu.io/api/edge/trending/anime');
 
-})
+        const trendingAnime = await response.data;
+
+        const animes = trendingAnime.data;
+
+        
+
+        res.render('homepage', {
+            animes,
+            // logged_in: req.session.logged_in
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// router.get('/login', (req, res) => {
+//     if (req.session.logged_in) {
+//         res.redirect('/');
+//         return;
+//     };
+    
+//     res.render('login');
+// });
+
+// router.get('/signup', (req, res) => {
+//     if(req.session.logged_in) {
+//         res.redirect('/');
+//         return;
+//     };
+
+//     res.render('signup');
+// });
 
 module.exports = router;
